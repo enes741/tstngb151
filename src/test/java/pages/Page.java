3404e31eb45110a391d5314f3a42c94a27b7abe6
Erol Evren <100449806+erlevren@users.noreply.github.com>
2023-07-24 22:00:09 +0300

    US001_V1

---
+++ src/test/java/pages/Page.java
@@ -0,0 +1,122 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Page {
+    public Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    //Burcu Hanım
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement aramaKutusu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Burcu Hanım
+    //Ayça Hanım
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Ayça Hanım
+
+
+
+    //Burcu Hanım
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Burcu Hanım
+}
